6cf9320061a42ec4c4e63e7d2709dcfd4f5ee1fd
test <koush0811@gmail.com>
2025-10-07 17:54:44 +0900

    classroom

---
+++ script.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  
+  
+});
+
