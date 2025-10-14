400bbfe687bd48609f3f30b7cf4ca91516d665e6
test <koush0811@gmail.com>
2025-10-14 15:54:38 +0900

    effect: 'cube'に変更と配置調整

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  //direction: 'vertical',
   loop: true,
   speed: 500,
+  effect: 'cube',
   autoplay: {
     delay: 5000,
     disableOnInteraction: false,
+    effect: 'cube',
   },
   // If we need pagination
   pagination: {
